b04caf00b6aa2db746e7c192627f500e7e771e43
Dong xiaonan <xndong@thoughtworks.com>
2014-10-15 15:58:41 +0530

    DXN-#20-Add my name to humans.txt

--- src/test/java/functional/com/trailblazers/freewheelers/HumansTxtTest.java
+++ src/test/java/functional/com/trailblazers/freewheelers/HumansTxtTest.java
@@ -33,8 +33,8 @@ public class HumansTxtTest {
                                         "Peter Gibbons",
                                         "Samir Nagheenanajar",
                                         "Michael Bolton",
-                                        "Juliana Paukowski"
-        };
+                                        "Juliana Paukowski",
+                                        "Xiaonan Dong"};
         assertThat(element.getText().split("\n"), is(expectedTeamMembers));
     }
 

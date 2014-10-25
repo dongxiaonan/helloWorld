30a82501b469d41389ff8bbcb6be9c898d832078
Mohammed Binsabbar <mbinsabb@eumbinsabb.local>
2014-10-25 10:47:49 +0530

    MB/KP - #0 - Make the logger private in AccountController

--- src/main/java/com/trailblazers/freewheelers/web/AccountController.java
+++ src/main/java/com/trailblazers/freewheelers/web/AccountController.java
@@ -24,7 +24,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/account")
 public class AccountController {
-    Logger logger;
+    private Logger logger;
 
     AccountService accountService;
     CountryService countryService;

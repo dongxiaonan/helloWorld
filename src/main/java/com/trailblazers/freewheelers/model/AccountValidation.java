74df51a4e53def60215b401be14c827e3c30071e
Dong xiaonan <xndong@thoughtworks.com>
2014-11-06 13:37:05 +0530

    GG/DXN Refactor : AccountValidation Class

--- src/main/java/com/trailblazers/freewheelers/model/AccountValidation.java
+++ src/main/java/com/trailblazers/freewheelers/model/AccountValidation.java
@@ -11,87 +11,79 @@ public class AccountValidation {
     private static final String CITY_FORMAT = "^[0-9a-zA-Z\\-\\'\\ \\.\\,]{1,100}$";
     private static final String STATE_FORMAT = "^[0-9a-zA-Z\\-\\'\\ \\.\\,]{0,100}$";
     private static final String PHONE_NUMBER_FORMAT = "[\\+(0-9)\\-]*";
+    private HashMap<String, String> errors = new HashMap<String, String>();
+    private Account account;
 
     public Map<String, String> verifyInputs(Account account) {
-        HashMap<String, String> errors = new HashMap<String, String>();
-
-        validateEmail(account, errors);
-
-        validateName(account, errors);
-
-        validateCountry(account, errors);
-
-        validateStreet1(account, errors);
-
-        validateStreet2(account, errors);
-
-        validateCity(account, errors);
-
-        validateState(account, errors);
-
-        validatePostcode(account, errors);
-
-        validatePhoneNumber(account, errors);
-
-        validatePassword(account, errors);
+        this.account = account;
+        validateEmail();
+        validateName();
+        validateCountry();
+        validateStreet1();
+        validateStreet2();
+        validateCity();
+        validateState();
+        validatePostcode();
+        validatePhoneNumber();
+        validatePassword();
         return errors;
     }
 
-    private void validatePassword(Account account, HashMap<String, String> errors) {
+    private void validatePassword() {
         if (!account.getPassword().matches(PASSWORD_FORMAT)){
             errors.put("password", "Must meet password requirement!");
         }
     }
 
-    private void validatePostcode(Account account, HashMap<String, String> errors) {
+    private void validatePostcode() {
         if(!account.getPostcode().matches("^[0-9a-zA-Z\\-]{4,10}$")) {
             errors.put("postcode","Must enter a post code!");
         }
     }
 
-    private void validateState(Account account, HashMap<String, String> errors) {
+    private void validateState() {
         if(!account.getState_Province().matches(STATE_FORMAT)) {
             errors.put("stateProvince","Must enter a valid state or province!");
         }
     }
 
-    private void validateCity(Account account, HashMap<String, String> errors) {
+    private void validateCity() {
         if(!account.getCity().matches(CITY_FORMAT)) {
             errors.put("city","Must enter a city!");
         }
     }
 
-    private void validateStreet2(Account account, HashMap<String, String> errors) {
+    private void validateStreet2() {
         if(!account.getStreet2().matches(STREET2_FORMAT)) {
             errors.put("street2","Enter a valid street!");
         }
     }
 
-    private void validateStreet1(Account account, HashMap<String, String> errors) {
+    private void validateStreet1() {
         if(!account.getStreet1().matches(STREET1_FORMAT)) {
             errors.put("street1", "Must enter a street!");
         }
     }
 
-    private void validateCountry(Account account, HashMap<String, String> errors) {
+    private void validateCountry() {
         if(CountryValidation.verifyInputs(account.getCountry()).containsKey("country")) {
             errors.putAll(CountryValidation.verifyInputs(account.getCountry()));
         }
     }
 
-    private void validateName(Account account, HashMap<String, String> errors) {
+    private void validateName() {
         if(account.getAccount_name().isEmpty()) {
             errors.put("name", "Must enter a name!");
         }
     }
 
-    private void validateEmail(Account account, HashMap<String, String> errors) {
+    private void validateEmail() {
         if (!account.getEmail_address().matches(EMAIL_FORMAT)) {
            errors.put("email", "Must enter a valid email!");
         }
     }
 
-    private void validatePhoneNumber(Account account, HashMap<String, String> errors) {
+    private void validatePhoneNumber() {
         boolean isPhoneNumberEmpty = account.getPhoneNumber().isEmpty();
         boolean doesPhoneNumberMatchRequirement = !account.getPhoneNumber().matches(PHONE_NUMBER_FORMAT);
 
